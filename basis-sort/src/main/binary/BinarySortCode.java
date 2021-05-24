113d64afb046212d4f2e2ca6986b717371dc19fc
mob <1225237693@qq.com>
2021-05-24 23:52:01 +0800

    二分查找

---
+++ basis-sort/src/main/binary/BinarySortCode.java
@@ -0,0 +1,76 @@
+package binary;
+
+import java.util.Arrays;
+
+/**
+ * 有序数组中找到num
+ *
+ * @author cmz
+ * @date 2021-05-24 22:42
+ */
+public class BinarySortCode {
+
+
+    public static boolean binarySortExist(int[] arr, int num) {
+
+        if (arr == null || arr.length == 0) {
+            return false;
+        }
+
+        int L = 0;
+        int R = arr.length - 1;
+
+        while (L <= R) {
+            int mid = (L + R) / 2;
+            if (arr[mid] == num) {
+                return true;
+            }
+            if (arr[mid] < num) {
+                L = mid + 1;
+            } else {
+                R = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
+    // for test
+    public static boolean test(int[] sortedArr, int num) {
+        for (int cur : sortedArr) {
+            if (cur == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // for test
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 10;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr = generateRandomArray(maxSize, maxValue);
+            Arrays.sort(arr);
+            int value = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+            if (test(arr, value) != binarySortExist(arr, value)) {
+                System.out.println("出错了！");
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+    }
+
+
+}
---
+++ basis-sort/src/main/binary/BinarySortLocalMinimumCode.java
@@ -0,0 +1,24 @@
+package binary;
+
+/**
+ * 局部最小值
+ * <p>
+ * arr 整体无序
+ * arr 相邻的数不相等
+ * LocalMin : arr[M-2] < arr[M-1]
+ *
+ * @author cmz
+ * @date 2021-05-24 23:23
+ */
+public class BinarySortLocalMinimumCode {
+
+
+    public static int binarySortLocalMinimum(int[] arr) {
+        if (arr == null || arr.length == 0) {
+
+        }
+        return -1;
+    }
+
+
+}
