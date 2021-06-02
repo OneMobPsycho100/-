d3069d56b5aa7bed0c69c96b4dd75e3cf8cb7150
mob <1225237693@qq.com>
2021-06-02 23:35:49 +0800

    位运算加减乘除

---
+++ basis-sort/src/main/t4_bitmap/BitAMMDCode02.java
@@ -0,0 +1,59 @@
+package t4_bitmap;
+
+/**
+ * BitAMMDCode02
+ *
+ * @author cmz
+ * @date 2021-06-02 23:27
+ */
+public class BitAMMDCode02 {
+
+    public static int add(int a, int b) {
+        int sum = a;
+        while (b != 0) {
+            sum = a ^ b;
+            b = (a & b) << 1;
+            a = sum;
+        }
+        return sum;
+    }
+
+    public static int negNum(int n) {
+        return add(~n, 1);
+    }
+
+    public static int minus(int a, int b) {
+        return add(a, negNum(b));
+    }
+
+    public static int multi(int a, int b) {
+        int ans = 0;
+        while (b != 0) {
+            if ((b & 1) != 0) {
+                ans = add(ans, a);
+            }
+            a <<= 1;
+            b >>>= 1;
+        }
+        return ans;
+    }
+
+
+    public static boolean isNeg(int n) {
+        return n < 0;
+    }
+
+    public static int div(int a, int b) {
+        int x = isNeg(a) ? negNum(a) : a;
+        int y = isNeg(b) ? negNum(b) : b;
+        int res = 0;
+        for (int i = 30; i >= 0; i = minus(i, 1)) {
+            if ((x >> i) >= y) {
+                res |= (1 << i);
+                x = minus(x, y << i);
+            }
+        }
+        return isNeg(a) ^ isNeg(b) ? negNum(res) : res;
+    }
+
+}
